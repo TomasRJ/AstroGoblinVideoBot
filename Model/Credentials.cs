683d6f00298a749b90105f7c50258847342bdb21
Tomas R J <49104467+TomasRJ@users.noreply.github.com>
2024-07-12 19:02:08 +0200

    Reddit client and secret not username and password

--- Model/Credentials.cs
+++ Model/Credentials.cs
@@ -3,6 +3,6 @@
 public readonly struct Credentials
 {
     public string HmacSecret { get; init; }
-    public string RedditUsername { get; init; }
-    public string RedditPassword { get; init; }
+    public string RedditClientId { get; init; }
+    public string RedditSecret { get; init; }
 }
