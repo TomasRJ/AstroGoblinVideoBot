7127d444f0dcb58f1177614fe0bc1e49d692bab4
Tomas R J <49104467+TomasRJ@users.noreply.github.com>
2024-07-13 00:20:16 +0200

    Add access_token response model

---
+++ Model/OauthToken.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AstroGoblinVideoBot.Model;
+
+public readonly struct OauthToken
+{
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; init; }
+    
+    [JsonPropertyName("token_type")]
+    public string TokenType { get; init; }
+    
+    [JsonPropertyName("expires_in")]
+    public int ExpiresIn { get; init; }
+    
+    [JsonPropertyName("scope")]
+    public string Scope { get; init; }
+}
