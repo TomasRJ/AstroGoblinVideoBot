4f6bd0b32e6c991f4c643410c6593fe302b7a58f
Tomas R J <49104467+TomasRJ@users.noreply.github.com>
2024-07-13 00:19:27 +0200

    Add config model

---
+++ Model/Config.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AstroGoblinVideoBot.Model;
+
+public struct Config
+{
+    [JsonPropertyName("AccessTokenUrl")]
+    public string AccessTokenUrl { get; init; }
+    
+    [JsonPropertyName("SubmitUrl")]
+    public string SubmitUrl { get; init; }
+    
+    [JsonPropertyName("UserAgent")]
+    public string UserAgent { get; init; }
+}
