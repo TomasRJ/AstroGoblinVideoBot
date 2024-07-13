45e174af1e3e977a14f163d64c8c0e28fa314440
Tomas R J <49104467+TomasRJ@users.noreply.github.com>
2024-07-13 04:30:47 +0200

    Add YoutubeSubscriber class the http post subscribe to the channel and verify

--- Program.cs
+++ Program.cs
@@ -5,25 +5,32 @@ using AstroGoblinVideoBot.Model;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-#region Credentials
-var userSecret = new ConfigurationBuilder()
-    .AddUserSecrets<Program>()
-    .Build()
-    .Get<Credentials>();
-var config = new ConfigurationBuilder()
-    .AddJsonFile("config.json")
-    .Build()
-    .Get<Config>();
-#endregion
+var config = new ConfigurationBuilder().AddJsonFile("config.json").Build().Get<Config>();
+
+await YoutubeSubscriber.SubscribeToChannel();
 
 var oauthToken = await RedditPoster.GetOauthToken();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/youtube", async youtubeVerify =>
+{
+    var query = youtubeVerify.Request.Query;
+    var topic = query["hub.topic"];
+    var challenge = query["hub.challenge"];
+    var mode = query["hub.mode"];
+    
+    if (!string.IsNullOrEmpty(challenge) && mode.Equals("subscribe") && topic.Equals(config.GooglePubSubTopic))
+    {
+        youtubeVerify.Response.ContentType = "text/plain";
+        await youtubeVerify.Response.WriteAsync(challenge!);
+    }
+    else
+        youtubeVerify.Response.StatusCode = 400;
+});
 
-app.MapPost("/", async context =>
+app.MapPost("/youtube", async youtubeRequest =>
 {
     var memoryStream = new MemoryStream();
-    await context.Request.Body.CopyToAsync(memoryStream);
+    await youtubeRequest.Request.Body.CopyToAsync(memoryStream);
     memoryStream.Position = 0;
     
     var serializer = new XmlSerializer(typeof(VideoFeed));
---
+++ YoutubeSubscriber.cs
@@ -0,0 +1,32 @@
+﻿using AstroGoblinVideoBot.Model;
+
+namespace AstroGoblinVideoBot;
+
+public abstract class YoutubeSubscriber
+{
+    private static readonly Config Config = new ConfigurationBuilder().AddJsonFile("config.json").Build().Get<Config>();
+    private static readonly Credentials UserSecret = new ConfigurationBuilder().AddUserSecrets<RedditPoster>().Build().Get<Credentials>();
+    private static readonly HttpClient YoutubeHttpClient = new();
+    
+    public static async Task<bool> SubscribeToChannel()
+    {
+        var subscribeForm = new FormUrlEncodedContent(new Dictionary<string, string>
+        {
+            { "hub.callback", Config.CallbackUrl },
+            { "hub.mode", "subscribe" },
+            { "hub.topic", Config.GooglePubSubTopic },
+            { "hub.verify", "async" },
+            { "hub.secret", UserSecret.HmacSecret }
+        });
+        var subscribeResponse = await YoutubeHttpClient.PostAsync(Config.GooglePubSubUrl, subscribeForm);
+        
+        if (subscribeResponse.IsSuccessStatusCode)
+        {
+            Console.WriteLine("Successfully subscribed to Youtube channel");
+            return true;
+        }
+        
+        Console.WriteLine("Failed to subscribe to Youtube channel");
+        return false;
+    }
+}
